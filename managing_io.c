969080cb4ce04ab2fbc48a49e2ec92de51f60faf
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2022-01-17 11:25:11 +0530

    Added Scanf

---
+++ managing_io.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    int n;
+    char c;
+
+
+    // Taking Input scanf()
+    printf("Enter Number n:");
+    scanf("%d",&n);
+
+    printf("Enter Character C :");
+    scanf("%c",&c);
+
+    
+}
